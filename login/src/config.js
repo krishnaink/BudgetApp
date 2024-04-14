1e92d243dff78c3ef8e544bce73a751ca64bf507
krishnaink <krishnakanth4477@gmail.com>
2024-04-14 02:04:52 -0400

    finish backend

--- login/src/config.js
+++ login/src/config.js
@@ -1,2 +1,26 @@
 const mongoose = require("mongoose");
-const connect = mongoose.connect("");
+const connect = mongoose.connect("mongodb+srv://krishnai27:Soccer%2F232@budgetapp.ogq5gvp.mongodb.net/login");
+
+//check database connected or not
+connect.then(() => {
+    console.log("Database connected sucessfully");
+})
+.catch(() => {
+    console.log("Failed to connect to database");
+});
+
+//create a schema
+const LoginSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    password: {
+        type: String,
+    }
+})
+
+//collection part
+const collection = new mongoose.model("users", LoginSchema);
+
+//import the model
+module.exports = collection;
--- login/src/index.js
+++ login/src/index.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const path = require('path');
 const bcrypt = require('bcrypt');
+const collection = require("./config");
 
 const app = express();
+//convert data into json format
+app.use(express.json());
 
-//Use ejs as the viw engine
+app.use(express.urlencoded({extended: false}));
+
+//Use ejs as the view engine
+//Routes
 app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) =>{
-    res.render("login");
+    res.render("home");
 
 })
-
 app.get("/signup", (req, res) =>{
     res.render("signup");
 })
@@ -22,6 +27,74 @@ app.get("/login", (req, res) =>{
     res.render("login");
 })
 
+app.get("/home", (req, res) =>{
+    res.render("home");
+})
+//End routes
+
+
+
+
+//register user for signup
+app.post("/signup", async (req, res) =>{
+
+    const data = {
+        name: req.body.username,
+        password: req.body.password
+    }
+
+    //check if user already exists in the database
+    const existingUser = await collection.findOne({name: data.name});
+    if(existingUser){
+        res.send("Username already exists! Please choose another username.")
+    }
+    //otherwise insert user data into database
+    else{
+        //hash the password using bcrypt
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(data.password, saltRounds);
+
+        //set password to hashed password
+        data.password = hashedPassword;
+
+        const userdata = await collection.insertMany(data);
+        console.log(userdata);
+
+        //send to app page
+        res.render("app");
+    }
+})
+
+//for users logging in
+app.post("/login", async (req, res) => {
+    try{
+        //check username in database
+        const check = await collection.findOne({name: req.body.username});
+        if(!check){
+            return res.send("Username not found!");
+        }
+
+        //check hashed password in database with the plain text passsword
+        const isPasswordMatched = await bcrypt.compare(req.body.password, check.password);
+        if(!isPasswordMatched){
+           return res.send("Wrong password!");
+        }
+        //successful login, send to app page
+        else{
+            return res.render("app");
+        }
+    }
+
+    catch{
+        console.log("Error in login");
+    }
+
+});
+
+
+
+
+//run on local host port 5000
 const port = 5000;
 app.listen(port, () => {
     console.log(`Server running on Port: $(port)`);
